6a0f3b38239760ce8afb6958e4261ab8039b924f
Yan Mingzhi <realraindust@gmail.com>
2021-05-20 20:52:32 +0800

    init tea pallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -41,6 +41,7 @@ use pallet_transaction_payment::CurrencyAdapter;
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
+pub use pallet_tea;
 /// Import the template pallet.
 pub use pallet_template;
 
@@ -276,6 +277,10 @@ impl pallet_template::Config for Runtime {
     type Event = Event;
 }
 
+impl pallet_tea::Config for Runtime {
+    type Event = Event;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
     pub enum Runtime where
@@ -293,6 +298,7 @@ construct_runtime!(
         Sudo: pallet_sudo::{Pallet, Call, Config<T>, Storage, Event<T>},
         // Include the custom logic from the pallet-template in the runtime.
         TemplateModule: pallet_template::{Pallet, Call, Storage, Event<T>},
+        TeaModule: pallet_tea::{Pallet, Call, Storage, Event<T>},
     }
 );
 
@@ -489,6 +495,7 @@ impl_runtime_apis! {
             add_benchmark!(params, batches, pallet_balances, Balances);
             add_benchmark!(params, batches, pallet_timestamp, Timestamp);
             add_benchmark!(params, batches, pallet_template, TemplateModule);
+            add_benchmark!(params, batches, pallet_tea, TeaModule);
 
             if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
             Ok(batches)
