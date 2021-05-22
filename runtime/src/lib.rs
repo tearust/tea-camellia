f0516590d2fb7dce2dad9c9a521c7c2df8f622ed
Yan Mingzhi <realraindust@gmail.com>
2021-05-22 15:16:06 +0800

    remote template pallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -41,9 +41,8 @@ use pallet_transaction_payment::CurrencyAdapter;
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
-pub use pallet_tea;
 /// Import the template pallet.
-pub use pallet_template;
+pub use pallet_tea;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -272,11 +271,6 @@ impl pallet_sudo::Config for Runtime {
     type Call = Call;
 }
 
-/// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
-    type Event = Event;
-}
-
 impl pallet_tea::Config for Runtime {
     type Event = Event;
 }
@@ -296,8 +290,7 @@ construct_runtime!(
         Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
         TransactionPayment: pallet_transaction_payment::{Pallet, Storage},
         Sudo: pallet_sudo::{Pallet, Call, Config<T>, Storage, Event<T>},
-        // Include the custom logic from the pallet-template in the runtime.
-        TemplateModule: pallet_template::{Pallet, Call, Storage, Event<T>},
+        // Include the custom logic from the pallets in the runtime.
         Tea: pallet_tea::{Pallet, Call, Config, Storage, Event<T>},
     }
 );
@@ -494,7 +487,6 @@ impl_runtime_apis! {
             add_benchmark!(params, batches, frame_system, SystemBench::<Runtime>);
             add_benchmark!(params, batches, pallet_balances, Balances);
             add_benchmark!(params, batches, pallet_timestamp, Timestamp);
-            add_benchmark!(params, batches, pallet_template, TemplateModule);
             add_benchmark!(params, batches, pallet_tea, TeaModule);
 
             if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
