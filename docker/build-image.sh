e212f473b85298fb62380eadec020b778e4d3262
Yan Mingzhi <realraindust@gmail.com>
2021-05-26 09:29:20 +0800

    update docker build to supprt fast mode

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+TEA_BUILD_MODE=$1
+: ${TEA_BUILD_MODE:="normal"}
 
 set -e
 
 echo "*** Start build tearust/tea-camellia:latest ***"
-bash ./docker/build.sh
+bash ./docker/build.sh $TEA_BUILD_MODE
 
 mkdir -p tmp
 cp ./docker/target/release/tea-camellia tmp
@@ -13,7 +15,9 @@ docker build -t tearust/tea-camellia:latest .
 cd ..
 rm -rf tmp
 
-if [ -n "$1" ]; then
-    docker tag tearust/tea-camellia:latest tearust/tea-camellia:$1
-    docker push tearust/tea-camellia:$1
+if [ -n "$2" ]; then
+    echo "*** Start tag and push tearust/tea-camellia:$2 ***"
+    docker tag tearust/tea-camellia:latest tearust/tea-camellia:$2
+    docker push tearust/tea-camellia:$2
 fi
+set +e
--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 # This script is meant to be run on Unix/Linux based systems
-set -e
+TEA_BUILD_MODE=$1
+: ${TEA_BUILD_MODE:="normal"}
+shift
 
-echo "*** Start build tea camellia ***"
+set -e
 
 cd $(dirname ${BASH_SOURCE[0]})/..
 
 docker compose down --remove-orphans
-docker compose run --rm build $@
+if [ $TEA_BUILD_MODE == "normal" ]; then
+    echo "*** Start build tea camellia: normal mode ***"
+    docker compose run --rm build $@
+elif [ $TEA_BUILD_MODE == "fast" ]; then
+    echo "*** Start build tea camellia: fast mode ***"
+    docker compose run --rm fast $@
+else
+    echo "unknown build mode, supported modes: normal, fast"
+fi
