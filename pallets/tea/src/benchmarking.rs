c7f3912a169b05c2e25ae4118dd119686d606ede
Yan Mingzhi <realraindust@gmail.com>
2021-05-27 16:59:22 +0800

    fix tea benchmark compile error

--- pallets/tea/src/benchmarking.rs
+++ pallets/tea/src/benchmarking.rs
@@ -1,16 +1,15 @@
 //! Benchmarking setup for pallet-tea
 
 use super::*;
-
 #[allow(unused)]
 use crate::Pallet as Template;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
-use hex_literal::hex;
 
 benchmarks! {
     add_new_node {
-        let public = hex!("e9889b1c54ccd6cf184901ded892069921d76f7749b6f73bed6cf3b9be1a8a44");
+        let mut public = [0; 32];
+        hex::decode_to_slice("e9889b1c54ccd6cf184901ded892069921d76f7749b6f73bed6cf3b9be1a8a44", &mut public as &mut [u8]).unwrap();
         let caller: T::AccountId = whitelisted_caller();
     }: _(RawOrigin::Signed(caller), public)
 }
