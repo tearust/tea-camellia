29fc789bead37bc1873e90377acdabaa8b28be89
Yan Mingzhi <realraindust@gmail.com>
2021-05-27 21:02:00 +0800

    let benchmark script file supprot generate template

--- benchmark.sh
+++ benchmark.sh
@@ -26,7 +26,41 @@ elif [ $PRIME_COMMAND = "weight" ]; then
     #--steps            # Number of samples across component ranges
     #--repeat           # Number of times we repeat a benchmark
     #--output           # Output benchmark results into a folder or file
-    ./target/release/tea-camellia benchmark --chain=dev --execution=wasm --wasm-execution=compiled --pallet="$PALLET_NAME" --extrinsic="$EXTRINSI_NAME" --steps=$BENCHMARK_STEP --repeat=$BENCHMARK_REPEAT --heap-pages=4096 --header=./file_header.txt --output="runtime/src/weights/${PALLET_NAME}.rs"
+    ./target/release/tea-camellia benchmark \
+        --chain=dev \
+        --execution=wasm \
+        --wasm-execution=compiled \
+        --pallet="$PALLET_NAME" \
+        --extrinsic="$EXTRINSI_NAME" \
+        --steps=$BENCHMARK_STEP \
+        --repeat=$BENCHMARK_REPEAT \
+        --heap-pages=4096 \
+        --header=./file_header.txt \
+        --output="runtime/src/weights/${PALLET_NAME}.rs"
+elif [ $PRIME_COMMAND = "template" ]; then
+    PALLET_NAME=$2
+    : ${PALLET_NAME:="pallet_tea"}
+    PACKAGE_NAME=$3
+    : ${PACKAGE_NAME:="tea"}
+    EXTRINSI_NAME=$4
+    : ${EXTRINSI_NAME:="*"}
+    BENCHMARK_STEP=$5
+    : ${BENCHMARK_STEP:=50}
+    BENCHMARK_REPEAT=$6
+    : ${BENCHMARK_REPEAT:=20}
+
+    ./target/release/tea-camellia benchmark \
+        --chain=dev \
+        --execution=wasm \
+        --wasm-execution=compiled \
+        --pallet="$PALLET_NAME" \
+        --extrinsic="$EXTRINSI_NAME" \
+        --steps=$BENCHMARK_STEP \
+        --repeat=$BENCHMARK_REPEAT \
+        --heap-pages=4096 \
+        --header="./file_header.txt" \
+        --output="pallets/${PACKAGE_NAME}/src/weights.rs" \
+        --template="./.maintain/frame-weight-template.hbs"
 else
     echo "unknown command, supported commands: build, test, weight"
 fi
