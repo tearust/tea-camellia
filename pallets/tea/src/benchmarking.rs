d6ba8187fc36c640510bc174cb996ade2adb431e
Yan Mingzhi <realraindust@gmail.com>
2021-05-27 15:17:36 +0800

    add benchmarks about pallets

--- pallets/tea/src/benchmarking.rs
+++ pallets/tea/src/benchmarking.rs
@@ -6,15 +6,13 @@ use super::*;
 use crate::Pallet as Template;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
+use hex_literal::hex;
 
 benchmarks! {
-    do_something {
-        let s in 0 .. 100;
+    add_new_node {
+        let public = hex!("e9889b1c54ccd6cf184901ded892069921d76f7749b6f73bed6cf3b9be1a8a44");
         let caller: T::AccountId = whitelisted_caller();
-    }: _(RawOrigin::Signed(caller), s)
-    verify {
-        assert_eq!(Something::<T>::get(), Some(s));
-    }
+    }: _(RawOrigin::Signed(caller), public)
 }
 
 impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test,);
