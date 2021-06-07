dfe7d8304d772981a3c9f0615717838c96242e01
kevingzhang <kevin.zhang.canada@gmail.com>
2021-06-07 12:40:23 -0700

    add unsafe external for ws and rpc

--- run-local.sh
+++ run-local.sh
@@ -1,4 +1,4 @@
 
 
 ./target/debug/tea-camellia purge-chain --dev
-./target/debug/tea-camellia --dev --alice --port 30334 --ws-port 9944 --rpc-port 9933
+./target/debug/tea-camellia --dev --alice --port 30334 --ws-port 9944 --rpc-port 9933 --unsafe-rpc-external --unsafe-ws-external
