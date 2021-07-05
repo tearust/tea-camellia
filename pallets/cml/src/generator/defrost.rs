a5a201d37669efac2e5048401baf8990137682e0
Yan Mingzhi <realraindust@gmail.com>
2021-07-05 08:31:11 +0800

    eliminate warning and rustfmt

--- pallets/cml/src/generator/defrost.rs
+++ pallets/cml/src/generator/defrost.rs
@@ -1,4 +1,6 @@
-use crate::param::{BLOCKS_IN_A_DAY, BLOCKS_IN_A_MONTH, UNFROZEN_SEEDS_PERCENTAGE_INVESTOR, BLOCKS_IN_HALF_MONTH };
+use crate::param::{
+	BLOCKS_IN_A_DAY, BLOCKS_IN_A_MONTH, BLOCKS_IN_HALF_MONTH, UNFROZEN_SEEDS_PERCENTAGE_INVESTOR,
+};
 use crate::DefrostScheduleType;
 use node_primitives::BlockNumber;
 use rand::{thread_rng, Rng};
@@ -20,19 +22,21 @@ pub fn make_generate_defrost_time_fn(
 
 		match defrost_schedule {
 			DefrostScheduleType::Investor => {
-				let prob = r % 100; 
-				if prob < UNFROZEN_SEEDS_PERCENTAGE_INVESTOR{
+				let prob = r % 100;
+				if prob < UNFROZEN_SEEDS_PERCENTAGE_INVESTOR {
 					//this seed fall into the the non-frozen 10%
 					0
 				} else {
-					let r:u32 = rng.gen();
+					let r: u32 = rng.gen();
 					let fall_in_month: u32 = r % 18;
-					fall_in_month * BLOCKS_IN_A_MONTH + BLOCKS_IN_HALF_MONTH + random_offset - 3 * BLOCKS_IN_A_DAY	// let rand_defrost_time_index = 18 as f32 // total eighteen months
+					fall_in_month * BLOCKS_IN_A_MONTH + BLOCKS_IN_HALF_MONTH + random_offset
+						- 3 * BLOCKS_IN_A_DAY // let rand_defrost_time_index = 18 as f32 // total eighteen months
 				}
 			}
 			DefrostScheduleType::Team => {
-				let fall_in_month:u32 = r % 20;
-				BLOCKS_IN_A_MONTH * (2 + fall_in_month) + BLOCKS_IN_HALF_MONTH + random_offset - 3 * BLOCKS_IN_A_DAY
+				let fall_in_month: u32 = r % 20;
+				BLOCKS_IN_A_MONTH * (2 + fall_in_month) + BLOCKS_IN_HALF_MONTH + random_offset
+					- 3 * BLOCKS_IN_A_DAY
 			}
 		}
 	}
@@ -49,7 +53,7 @@ mod tests {
 
 		let test_sample_count = 1000;
 		let mut distribute = [0u16; 36 * 30];
-		let mut month_distribute = [0u16;24];
+		let mut month_distribute = [0u16; 24];
 		for _i in 0..test_sample_count {
 			let defrost_time = closure_defrost_time();
 			let defrost_days = (defrost_time / BLOCKS_IN_A_DAY) as usize;
@@ -67,8 +71,7 @@ mod tests {
 			println!("month {} seeds {}", i, month_distribute[i]);
 		}
 		println!("Investor defrost ...");
-		let mut distribute = [0u16; 36 * 30];
-		let mut month_distribute = [0u16;24];
+		let mut month_distribute = [0u16; 24];
 		let closure_defrost_time = make_generate_defrost_time_fn(DefrostScheduleType::Investor);
 
 		let test_sample_count = 1000;
