116e9bb02e32f4e7730435e3589343eb35910a54
Yan Mingzhi <realraindust@gmail.com>
2021-07-13 15:22:53 +0800

    clean meanless files under root directory

--- fast.sh
+++
@@ -1,4 +0,0 @@
-
-
-./target/debug/tea-camellia purge-chain --chain local
-./target/debug/tea-camellia --chain local --alice --port 30334 --ws-port 9944 --rpc-port 9933 --unsafe-rpc-external --unsafe-ws-external
