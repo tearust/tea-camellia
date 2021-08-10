499ba4a8323cb5ee5fee0c4f6efc31c54e4ed244
Yan Mingzhi <realraindust@gmail.com>
2021-08-10 15:27:27 +0800

    fix errors about reversing withdraw and deposit calculation

--- pallets/genesis-exchange/src/tests.rs
+++ pallets/genesis-exchange/src/tests.rs
@@ -45,15 +45,44 @@ fn tea_to_usd_works() {
 }
 
 #[test]
+fn tea_to_usd_works_if_withdraw_all_remains_usd() {
+	new_test_ext().execute_with(|| {
+		let user = 1;
+		let tea_amount = 100090 * 10_000_000_000;
+		<Test as Config>::Currency::make_free_balance_be(&user, tea_amount);
+
+		assert_ok!(GenesisExchange::tea_to_usd(
+			Origin::signed(user),
+			tea_amount
+		));
+		assert_eq!(<Test as Config>::Currency::free_balance(&user), 0);
+
+		// deposit to exchange again
+		<Test as Config>::Currency::make_free_balance_be(&user, tea_amount);
+		assert_ok!(GenesisExchange::tea_to_usd(
+			Origin::signed(user),
+			tea_amount
+		));
+		assert_eq!(<Test as Config>::Currency::free_balance(&user), 0);
+	})
+}
+
+#[test]
 fn tea_to_usd_should_fail_if_withdraw_amount_larger_than_exchange_really_has() {
 	new_test_ext().execute_with(|| {
+		let user = 1;
 		assert_eq!(
 			USDStore::<Test>::get(OperationAccount::<Test>::get()),
 			OPERATION_USD_AMOUNT
 		);
+		<Test as Config>::Currency::make_free_balance_be(&user, OPERATION_USD_AMOUNT + 1);
 		assert_noop!(
-			GenesisExchange::tea_to_usd(Origin::signed(1), OPERATION_USD_AMOUNT + 1),
-			Error::<Test>::ExchangeInsufficientUSD
+			GenesisExchange::tea_to_usd(Origin::signed(user), OPERATION_USD_AMOUNT + 1),
+			Error::<Test>::ExchangeInsufficientTEA
+		);
+		assert_noop!(
+			GenesisExchange::tea_to_usd(Origin::signed(user), OPERATION_USD_AMOUNT),
+			Error::<Test>::ExchangeInsufficientTEA
 		);
 	})
 }
@@ -124,15 +153,45 @@ fn usd_to_tea_works() {
 }
 
 #[test]
+fn usd_to_tea_works_if_withdraw_all_remains_usd() {
+	new_test_ext().execute_with(|| {
+		let user = 1;
+		let usd_amount = 100090 * 10_000_000_000;
+		USDStore::<Test>::insert(user, usd_amount);
+
+		assert_ok!(GenesisExchange::usd_to_tea(
+			Origin::signed(user),
+			usd_amount
+		));
+		assert_eq!(USDStore::<Test>::get(user), 0);
+
+		// deposit to exchange again
+		USDStore::<Test>::insert(user, usd_amount);
+
+		assert_ok!(GenesisExchange::usd_to_tea(
+			Origin::signed(user),
+			usd_amount
+		));
+		assert_eq!(USDStore::<Test>::get(user), 0);
+	})
+}
+
+#[test]
 fn usd_to_tea_should_fail_if_withdraw_amount_larger_than_exchange_really_has() {
 	new_test_ext().execute_with(|| {
+		let user = 1;
 		assert_eq!(
 			<Test as Config>::Currency::free_balance(&OperationAccount::<Test>::get()),
 			OPERATION_TEA_AMOUNT
 		);
+		USDStore::<Test>::insert(user, OPERATION_USD_AMOUNT + 1);
 		assert_noop!(
-			GenesisExchange::usd_to_tea(Origin::signed(1), OPERATION_USD_AMOUNT + 1),
-			Error::<Test>::ExchangeInsufficientTEA
+			GenesisExchange::usd_to_tea(Origin::signed(user), OPERATION_USD_AMOUNT + 1),
+			Error::<Test>::ExchangeInsufficientUSD
+		);
+		assert_noop!(
+			GenesisExchange::usd_to_tea(Origin::signed(user), OPERATION_USD_AMOUNT),
+			Error::<Test>::ExchangeInsufficientUSD
 		);
 	})
 }
