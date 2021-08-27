9a8fe7943090fb3d5910af224a624c04e9d5ae31
kevin.zhang.canada@gmail.com <kevin.zhang.canada@gmail.com>
2021-08-26 17:41:32 -0700

    update math reverse

--- pallets/bounding-curve/curve-impl/src/square_root.rs
+++ pallets/bounding-curve/curve-impl/src/square_root.rs
@@ -29,13 +29,21 @@ where
 		times: &mut usize,
 	) -> Balance {
 		*times += 1;
-
+		println!("xn {:?}", &x_n);
+		// println!(
+		// 	"parta {:?}",
+		// 	area.clone() / K.into() * 10u32.into() * 1_000_000u32.into() / x_n.integer_sqrt()
+		// );
+		// println!("partb {:?}", x_n.clone() * 2u32.into() / 3u32.into());
 		let result = if x_n.is_zero() {
 			Zero::zero()
 		} else {
-			x_n.clone() - x_n.clone() * 2u32.into() * 1_000_000u32.into() * 1_000_000u32.into()/ 3u32.into() 
+			x_n.clone()
 				+ area.clone() / K.into() * 10u32.into() / 1_000_000u32.into() / x_n.integer_sqrt()
+				- x_n.clone() * 2u32.into() / 3u32.into() / 1_000_000u32.into() / 1_000_000u32.into()
 		};
+
+		// println!("result {:?}", &result);
 
 		if approximately_equals(x_n, result.clone(), precision.clone()) {
 			result
@@ -44,70 +52,85 @@ where
 		}
 	}
 
-	fn select_nearest_area_and_x(current_area: Balance) -> (Balance, Balance) {
-		let select_fn = |it: &[u32; AREA_LIST_LENGTH]| {
-			let mut best_area: u32 = 0;
-			let mut best_x: u32 = 0;
-			for (i, area) in it.iter().enumerate() {
-				if current_area < Balance::from(*area) {
-					break;
-				}
+	// fn select_nearest_area_and_x(current_area: Balance) -> (Balance, Balance) {
+	// 	let select_fn = |it: &[u32; AREA_LIST_LENGTH]| {
+	// 		let mut best_area: u32 = 0;
+	// 		let mut best_x: u32 = 0;
+	// 		for (i, area) in it.iter().enumerate() {
+	// 			if current_area < Balance::from(*area) {
+	// 				break;
+	// 			}
 
-				best_area = *area;
-				if i.is_zero() {
-					best_x = 1;
-				} else {
-					best_x = 100 * i as u32;
-				}
-			}
+	// 			best_area = *area;
+	// 			if i.is_zero() {
+	// 				best_x = 1;
+	// 			} else {
+	// 				best_x = 100 * i as u32;
+	// 			}
+	// 		}
 
-			(Balance::from(best_area), Balance::from(best_x))
-		};
+	// 		(Balance::from(best_area), Balance::from(best_x))
+	// 	};
 
-		match K {
-			1000 => select_fn(&K10_STEP100_AREA_LIST),
-			700 => select_fn(&K7_STEP100_AREA_LIST),
-			_ => (Self::pool_balance(One::one()), One::one()),
-		}
-	}
+	// 	match K {
+	// 		1000 => select_fn(&K10_STEP100_AREA_LIST),
+	// 		700 => select_fn(&K7_STEP100_AREA_LIST),
+	// 		_ => (Self::pool_balance(One::one()), One::one()),
+	// 	}
+	// }
 }
 
 impl<Balance, const K: u32> BoundingCurveInterface<Balance> for UnsignedSquareRoot<Balance, K>
 where
 	Balance: AtLeast32BitUnsigned + Default + Clone + Debug,
 {
+	/// Input unit 1E12 is one token. Output is unit 1E12 is one TEA
 	fn buy_price(total_supply: Balance) -> Balance {
-		// total_supply.integer_sqrt() * K.into() / K_BASE_POINT.into()
-		total_supply.integer_sqrt() * K.into() / 10u32.into() * 1000000u32.into()
+		total_supply.integer_sqrt() * K.into() / 10u32.into() * 1_000_000u32.into()
 	}
 
+	/// Input unit 1E12 is one token. Output is unit 1E12 is one TEA
 	fn pool_balance(x: Balance) -> Balance {
 		x.integer_sqrt() * x.clone() * K.into() * 2u32.into() / 1_000_000u32.into() / 30u32.into()
 	}
-
+	/// Input unit 1E12 is one token. Output is unit 1E12 is one TEA
 	fn pool_balance_reverse(area: Balance, precision: Balance) -> Balance {
 		if area.is_zero() {
 			return Zero::zero();
 		}
 
-		let (seed_area, seed_x) = Self::select_nearest_area_and_x(area.clone());
-		if area == seed_area {
-			return seed_x;
-		}
+		// let (seed_area, seed_x) = Self::select_nearest_area_and_x(area.clone());
+		// if area == seed_area {
+		// 	return seed_x;
+		// }
 
 		let mut times = 0;
-		let result = Self::recursively_balance_reverse_calculation(
-			seed_x,
-			area.clone(),
-			precision,
-			&mut times,
-		);
-		#[cfg(feature = "std")]
-		println!(
-			"area {:?} (K: {}) calculated result is {:?}, calculated times: {}",
-			area, K, result, times
-		);
-		result
+		let mut x_n: Balance = Balance::from(1_100_000u32) * Balance::from(1_000_000u32);
+		loop {
+			let x_n_plus_1: Balance = {
+				if x_n.is_zero() {
+					Zero::zero()
+				} else {
+					x_n.clone()
+						+ area.clone() / K.into() * 10u32.into() * 1_000_000u32.into() / x_n.integer_sqrt()
+						- x_n.clone() * 2u32.into() / 3u32.into()
+				}
+			};
+			// println!("times is {}, xnplusone is {:?}", &times, &x_n_plus_1);
+			if approximately_equals(x_n.clone(), x_n_plus_1.clone(), precision.clone()) {
+				#[cfg(feature = "std")]
+				println!("exiting with {} loops", times);
+				return x_n_plus_1;
+			} else {
+				x_n = x_n_plus_1;
+				times += 1;
+			}
+			// println!(
+			// 	"area {:?} (K: {}) calculated result is {:?}, calculated times: {}",
+			// 	area, K, result, times
+			// );
+		}
+		// result
 	}
 }
 
@@ -189,85 +212,138 @@ mod tests {
 			<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance(100 * DOLLARS),
 			466666666666666
 		);
-	} 
-
+	}
 	#[test]
-	fn pool_balance_reverse_works() {
+	fn kevin() {
 		#[allow(non_camel_case_types)]
 		type RootSquare_10 = UnsignedSquareRoot<Balance, 10>; // y = 10√x
-		assert_eq!(
-			<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(0, 1),
-			0
-		);
-		assert!(approximately_equals(
-			<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(66, 1),
-			1000000,
-			7000
-		));
-		assert_eq!(
-			<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(2108, 1),
-			10000000
-		);
-		assert!(approximately_equals(
-			<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(66666, 100000),
-			100000000,
-			6000
-		));
-		assert!(approximately_equals(
-			<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(
-				66666666666,
-				100000
-			),
-			DOLLARS,
-			20000
-		));
-		assert!(approximately_equals(
-			<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(
-				66666666666666,
-				100000
-			),
-			100 * DOLLARS,
-			35000
-		));
-
-		#[allow(non_camel_case_types)]
 		type RootSquare_7 = UnsignedSquareRoot<Balance, 7>; // y = 7√x
-		assert_eq!(
-			<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(0, 1),
-			0
+		println!("Kevin say hi");
+		let x = <RootSquare_10 as BoundingCurveInterface<Balance>>::buy_price(DOLLARS);
+		println!("K10 buy one token use T:{:?}/10000", &x / 100000000);
+		let x = <RootSquare_10 as BoundingCurveInterface<Balance>>::buy_price(100 * DOLLARS);
+		println!("K10 buy 100 token use T:{:?}/10000", &x / 100000000);
+		let x = <RootSquare_7 as BoundingCurveInterface<Balance>>::buy_price(DOLLARS);
+		println!("K7 buy one token use T:{:?}/10000", &x / 100000000);
+		let x = <RootSquare_7 as BoundingCurveInterface<Balance>>::buy_price(100 * DOLLARS);
+		println!("K7 buy 100 token use T:{:?}/10000", &x / 100000000);
+		let x = <RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance(DOLLARS);
+		println!(
+			"K10 when supply is 1 token, pool balance T is {:?}/10000",
+			x / 100000000 // <RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance(DOLLARS)
 		);
-		assert!(approximately_equals(
-			<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(46, 1),
-			1000000,
-			10000,
-		));
-		assert!(approximately_equals(
-			<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(1475, 100000),
-			10000000,
-			3000
-		));
-		assert!(approximately_equals(
-			<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(46666, 100000),
-			100000000,
-			5500
-		));
-		assert!(approximately_equals(
-			<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(
-				46666666666,
-				100000
-			),
-			DOLLARS,
-			20000
-		));
-		assert!(approximately_equals(
-			<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(
-				46666666666666,
-				100000
-			),
-			100 * DOLLARS,
-			35000
-		));
+		println!(
+			"K10 now let us find how much token can receive when spending {:?}/10000 TEA. answer is {:?}/10000",
+			x / 100000000,
+			<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(x, 10) / 100000000
+		);
+		let x = <RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance(100 * DOLLARS);
+		println!(
+			"K10 when supply is 100 token, pool balance T is {:?}/10000",
+			x / 100000000
+		);
+		println!(
+			"K10  now let us find how much token can receive when spending {:?}/10000 TEA. answer is {:?}/10000",
+			x / 100000000,
+			<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(x, 10)
+				/ 100000000
+		);
+
+		let x = <RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance(DOLLARS);
+		println!(
+			"K7 when supply is 1 token, pool balance T is {:?}/10000",
+			x / 100000000 // <RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance(DOLLARS)
+		);
+		println!(
+			"K7 now let us find how much token can receive when spending {:?}/10000 TEA. answer is {:?}/10000",
+			x / 100000000,
+			<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(x, 10)
+				/ 100000000
+		);
+		let x = <RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance(100 * DOLLARS);
+		println!(
+			"K7 when supply is 100 token, pool balance T is {:?}/10000",
+			x / 100000000
+		);
+		println!(
+			"K7  now let us find how much token can receive when spending {:?}/10000 TEA. answer is {:?}/10000",
+			x / 100000000,
+			<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(x, 10) / 100000000
+		);
 	}
+
+	// 666666666666
+	// #[test]
+	// fn pool_balance_reverse_works() {
+	// 	#[allow(non_camel_case_types)]
+	// 	type RootSquare_10 = UnsignedSquareRoot<Balance, 10>; // y = 10√x
+	// 	assert_eq!(
+	// 		<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(0, 1),
+	// 		0
+	// 	);
+	// 	assert!(approximately_equals(
+	// 		<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(66, 1),
+	// 		1000000,
+	// 		7000
+	// 	));
+	// 	assert_eq!(
+	// 		<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(2108, 1),
+	// 		10000000
+	// 	);
+	// 	assert!(approximately_equals(
+	// 		<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(66666, 100000),
+	// 		100000000,
+	// 		6000
+	// 	));
+	// 	assert!(approximately_equals(
+	// 		<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(66666666666, 100000),
+	// 		DOLLARS,
+	// 		20000
+	// 	));
+	// 	assert!(approximately_equals(
+	// 		<RootSquare_10 as BoundingCurveInterface<Balance>>::pool_balance_reverse(
+	// 			66666666666666,
+	// 			100000
+	// 		),
+	// 		100 * DOLLARS,
+	// 		35000
+	// 	));
+
+	// 	#[allow(non_camel_case_types)]
+	// 	type RootSquare_7 = UnsignedSquareRoot<Balance, 7>; // y = 7√x
+	// 	assert_eq!(
+	// 		<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(0, 1),
+	// 		0
+	// 	);
+	// 	assert!(approximately_equals(
+	// 		<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(46, 1),
+	// 		1000000,
+	// 		10000,
+	// 	));
+	// 	assert!(approximately_equals(
+	// 		<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(1475, 100000),
+	// 		10000000,
+	// 		3000
+	// 	));
+	// 	assert!(approximately_equals(
+	// 		<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(46666, 100000),
+	// 		100000000,
+	// 		5500
+	// 	));
+	// 	assert!(approximately_equals(
+	// 		<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(46666666666, 100000),
+	// 		DOLLARS,
+	// 		20000
+	// 	));
+	// 	assert!(approximately_equals(
+	// 		<RootSquare_7 as BoundingCurveInterface<Balance>>::pool_balance_reverse(
+	// 			46666666666666,
+	// 			100000
+	// 		),
+	// 		100 * DOLLARS,
+	// 		35000
+	// 	));
+	// }
 
 	#[test]
 	fn buy_and_sell_price_works() {
