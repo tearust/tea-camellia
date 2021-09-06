9d776c51f8b1b8eeb680cd3aedb9f6fc92fed29a
Yan Mingzhi <realraindust@gmail.com>
2021-09-06 16:52:41 +0800

    fix tea unit test error

--- pallets/tea/src/tests.rs
+++ pallets/tea/src/tests.rs
@@ -1,31 +1,18 @@
 use crate::{mock::*, types::*, BuiltinNodes, Error, Nodes};
 use frame_support::{assert_noop, assert_ok};
 use hex_literal::hex;
+use tea_interface::TeaOperation;
 
 #[test]
 fn add_new_node_works() {
 	new_test_ext().execute_with(|| {
 		let public: [u8; 32] =
 			hex!("e9889b1c54ccd6cf184901ded892069921d76f7749b6f73bed6cf3b9be1a8a44");
-		assert_ok!(Tea::add_new_node(Origin::signed(1), public));
+		Tea::add_new_node(public, &1);
 		let target_node = Nodes::<Test>::get(&public).unwrap();
 		assert_eq!(
 			target_node.create_time,
 			frame_system::Pallet::<Test>::block_number()
-		);
-	})
-}
-
-#[test]
-fn add_new_node_already_exist() {
-	new_test_ext().execute_with(|| {
-		let public: [u8; 32] =
-			hex!("e9889b1c54ccd6cf184901ded892069921d76f7749b6f73bed6cf3b9be1a8a44");
-		let _ = Tea::add_new_node(Origin::signed(1), public);
-
-		assert_noop!(
-			Tea::add_new_node(Origin::signed(1), public),
-			Error::<Test>::NodeAlreadyExist
 		);
 	})
 }
