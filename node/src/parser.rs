0384699b909be0505e734da9a908389241c3f722
Yan Mingzhi <realraindust@gmail.com>
2022-07-15 17:49:34 +0800

    remove mining startup settings

--- node/src/parser.rs
+++ node/src/parser.rs
@@ -11,19 +11,6 @@ impl Cli {
 		}
 	}
 
-	pub fn parse_mining_startup(&self) -> Result<Vec<([u8; 32], u64, Vec<u8>)>, String> {
-		match self.mining_startup_path.as_ref() {
-			Some(path) => {
-				let mut rdr = csv::Reader::from_path(path).map_err(|e| e.to_string())?;
-				parse_mining_startup_config(&mut rdr)
-			}
-			None => {
-				let mut rdr = csv::Reader::from_reader(&include_bytes!("mining_startup.csv")[..]);
-				parse_mining_startup_config(&mut rdr)
-			}
-		}
-	}
-
 	pub fn parse_tapp_startup(&self) -> Result<Vec<([u8; 32], u64, Vec<u8>)>, String> {
 		match self.tapp_startup_path.as_ref() {
 			Some(path) => {
@@ -47,29 +34,6 @@ fn seed_from_string(s: &str) -> [u8; 32] {
 		seed[i] = str_bytes[i];
 	}
 	seed
-}
-
-fn parse_mining_startup_config<R>(
-	rdr: &mut csv::Reader<R>,
-) -> Result<Vec<([u8; 32], u64, Vec<u8>)>, String>
-where
-	R: std::io::Read,
-{
-	const MACHINE_ID_INDEX: usize = 0;
-	const CML_ID_INDEX: usize = 1;
-	const CONN_ID_INDEX: usize = 2;
-
-	let mut startup_list = Vec::new();
-	for record in rdr.records() {
-		let record = record.map_err(|e| e.to_string())?;
-		let machine_id = parse_machine_id(record.get(MACHINE_ID_INDEX))?;
-		let cml_id = parse_u64(record.get(CML_ID_INDEX), "cml id")?;
-		let conn_id = parse_utf8_encoded(record.get(CONN_ID_INDEX), "conn id")?;
-
-		startup_list.push((machine_id, cml_id, conn_id));
-	}
-
-	Ok(startup_list)
 }
 
 fn parse_tapp_startup_config<R>(
